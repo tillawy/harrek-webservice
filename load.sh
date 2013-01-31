62ebe49c11de8a0065fdd9d9fc691a6d0dc12f93
Mohammad O. Tillawy <tillawy@gmail.com>
2013-01-31 23:40:40 +0300

    a script to reload the json into the iphone app

---
+++ load.sh
@@ -0,0 +1,4 @@
+echo "loading  values into LettersUIKit"
+php json.php  > /Users/mohammed/works/IOSProjects/Letters-UIKit/Letters-UIKitTests/poet_2.json 
+php families.php  > /Users/mohammed/works/IOSProjects/Letters-UIKit/Letters-UIKitTests/families.json 
+php v.php  > /Users/mohammed/works/IOSProjects/Letters-UIKit/Letters-UIKitTests/Vocabulary.json 
