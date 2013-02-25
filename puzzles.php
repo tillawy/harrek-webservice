7c5dd29ff8e407d65c0cbf0058f7593b120be60b
Mohammad O. Tillawy <tillawy@gmail.com>
2013-02-25 22:54:40 +0300

    adding a file to serve available puzzles

---
+++ puzzles.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('./require.php');
+
+header('Content-Type: application/json; charset=utf-8');
+
+
+$dir    = '/Users/mohammed/Sites/letters/Puzzles/';
+
+$files1 = scandir($dir);
+$exclude_list = array(".", "..", "example.txt");
+$files2 = array_diff( $files1 , $exclude_list);
+//print_r( $files2 );
+$entries = [];
+
+foreach ( $files2 as $file ){
+		  if (preg_match ("/^[0-9]*\.txt/",$file) ){
+					 $entries []= [ "id" =>  preg_replace( "/\..*$/" , "" , $file ) ];
+		  }
+}
+
+echo( json_encode($entries) );
+
+
+?>
