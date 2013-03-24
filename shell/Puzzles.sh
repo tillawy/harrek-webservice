2a3e5d90546993ecafd7c6c7d949a59688b32427
Mohammad O. Tillawy <tillawy@gmail.com>
2013-03-24 21:29:26 +0300

    classify puzzles

---
+++ shell/Puzzles.sh
@@ -0,0 +1,18 @@
+
+rm -f ./Puzzles/*
+mkdir  ./Puzzles
+i=0;
+ls -flrt -d1  ./PuzzlesClassified/intro/* ./PuzzlesClassified/easy/* ./PuzzlesClassified/medium/* ./PuzzlesClassified/hard/* | while read file; do
+
+		  oldFileName=${file};
+		  newFileName=$(printf "%03d" ${i}).txt;
+		  echo "old:${oldFileName} -> new:${newFileName}";
+		  echo cp ${oldFileName}  ./Puzzles/${newFileName};
+		  cp ${oldFileName}  ./Puzzles/${newFileName};
+		  ls ./Puzzles/${newFileName};
+		  chmod 775 ./Puzzles/${newFileName};
+		  i=$(( i + 1 ));
+
+done
+
+cp PuzzlesClassified/tutorials/tutorial0.txt ./Puzzles/ 
