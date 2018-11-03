78a052a2ffb7f51a31c38aae9305e67d430e2eb4
Mohammed O. Tillawy <tillawy@gmail.com>
2018-11-03 10:59:13 +0200

    cleanup push script

--- docker/docker.build.remote.and.push.sh
+++ docker/docker.build.remote.and.push.sh
@@ -4,14 +4,14 @@ docker login registry.docker.easydisplay.info
 
 readonly APP_VERSION=$(cat composer.json | grep version | cut -f 4 -d '"');
 
-readonly IMAGE_NAME="harrek/webservice:${APP_VERSION}";
+readonly IMAGE_NAME="harrek/webservice";
 
 docker build --tag ${IMAGE_NAME} --file ./Dockerfile .;
 
 readonly REGISTRY="registry.docker.easydisplay.info";
 
-echo "\n\n\n\n\t\t • tagging: ${IMAGE_NAME} as: ${REGISTRY}/${IMAGE_NAME} \n\n\n\n";
+echo "\n\n\n\n\t\t • tagging: ${IMAGE_NAME} as: ${REGISTRY}/${IMAGE_NAME}:${APP_VERSION} \n\n\n\n";
 
-docker tag ${IMAGE_NAME} ${REGISTRY}/${IMAGE_NAME};
+docker tag ${IMAGE_NAME} ${REGISTRY}/${IMAGE_NAME}:${APP_VERSION};
 
-docker push ${REGISTRY}/${IMAGE_NAME};
+docker push ${REGISTRY}/${IMAGE_NAME}:${APP_VERSION};
